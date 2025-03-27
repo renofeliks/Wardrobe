784e3346f0ebb64f7f443efd4e1d62c5f8d476bd
Reno <renofeliks.lindvere@gmail.com>
2025-03-27 15:45:12 +0200

    Minor removal

--- backend/database.js
+++ backend/database.js
@@ -2,7 +2,7 @@ const Pool = require('pg').Pool;
 
 const pool = new Pool({
     user: "postgres",
-    password: "HarmoRenoRon",
+    password: "",
     database: "Wardrobe",
     host: "localhost",
     port: "5433"
